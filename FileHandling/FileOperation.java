7ed9a30770d961f43f3e23ecacd83dc6825e441e
Jinendra Bafna <jinendra.bafna@neutrinotechlabs.com>
2023-07-10 14:39:30 +0530

    file

--- FileHandling/FileOperation.java
+++ FileHandling/FileOperation.java
@@ -15,10 +15,64 @@ public class FileOperation
             System.out.println("Txt File Not Found");
         }
     }
+    public static void writeFile() {
+        try {
+            FileWriter file = new FileWriter("C:\\Users\\NTS-Jinendra Bafna\\Desktop\\jay.txt");
+            file.write(" This is new File Affter write New Data");
+            file.close();
+            System.out.println("File Write Successfully");
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+
+        public static void createFile()
+    {
+        try {
+
+            File obj = new File("test1.txt");
+            obj.createNewFile();
+
+            System.out.println("File Create Successfully");
+
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+    public  static  void delete()
+    {
+        File obj = new File("C:\\Users\\NTS-Jinendra Bafna\\Desktop\\jay.txt");
+        System.out.println(obj.delete()+" File Deleted Successfully");
+
+    }
+
+
+
 
     public static void main(String[] args)
     {
-        FileOperation.readFile();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Press 1 for readFile");
+        System.out.println("Press 2 for writeFile");
+        System.out.println("Press 3 for deleteFile");
+        System.out.println("Press 4 for createFile");
+
+        int input =sc.nextInt();
+        switch (input){
+            case 1:
+                FileOperation.readFile();
+                break;
+            case 2:
+                FileOperation.writeFile();
+                break;
+            case 3:
+                FileOperation.delete();
+                break;
+            case 4:
+                FileOperation.createFile();
+                break;
+        }
 
     }
 
